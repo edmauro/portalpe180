d65c50670f697f2dcd7b2b2c41d60444afa46b49
edmauro <edmauro.morais@hotmail.com>
2013-09-19 19:39:33 -0300

    comite teste

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-BR">
+	<head>
+		<title>Título do Blog</title>
+		<link rel="stylesheet" type="text/css" media="all" href="/iniciotemplate/style.css" />
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="Naver-master/jquery.fs.naver.min.js"></script>
+		<script>
+			$("naver").naver();
+		</script>
+		
+	</head>
+
+	<body>
+		
+		<div id="page-wrap">
+			
+			<div id="header">
+				<p><a href="" title="Nome do Blog">Nome do Blog</a></p>
+				<span><a href="" title="Descrição do Blog">Descrição do Blog</a></span>
+			</div>
+			
+			<div class="naver">
+				<ul>
+					<li><a href="" title="Item">Item 1</a></li>
+					<li><a href="" title="Item">Item 2</a></li>
+					<li><a href="" title="Item">Item 3</a></li>
+					<li><a href="" title="Item">Item 4</a></li>
+					<li><a href="" title="Item">Item 5</a></li>
+				</ul>
+			</div>
+			
+			<div id="container">
+
+				<div id="content">
+				
+					<div class="post">
+						<h1><a href="" title="Olá, mundo!">Olá, mundo!</a></h1>
+
+						<div class="entry-meta">
+							<p>Escrito em:
+								<a href="" title="30/10/2010">30 de outubro de 2010</a> por
+								<a href="" title="Mais posts de mazetto">mazetto</a>
+							</p>
+						</div> 
+
+													
+						<div class="entry"> 
+							<p>Bem-vindo ao WordPress. Esse é o seu primeiro post. Edite-o ou exclua-o, e aí comece a brincadeira!</p> 
+						</div> 
+						
+														
+						<div class="post-utility"> 
+							<p>
+								Categoria:
+								<a href="" title="Ver todos os posts de Atualizações">Atualizações</a>
+								| Tags:
+								<a href="" title="Tag">tag1</a>,
+								<a href="" title="Tag">tag2</a>,
+								<a href="" title="Tag">tag3</a>
+							</p>
+						</div> 
+						
+					</div><!-- .post -->
+ 
+											
+				</div><!-- #content -->
+ 
+				<div id="sidebar"> 
+					<ul> 
+						<li>
+							<form method="get" id="searchform" action=""> 
+								<div>
+									<input type="text" value="" name="s" id="s" /> 
+									<input type="submit" id="searchsubmit" value="Buscar" /> 
+								</div> 
+							</form>
+						</li> 
+					</ul>
+					<ul> 
+						<li><h3>Arquivo</h3></li> 
+						<li>
+							<ul>
+								<li><a href='' title="outubro 2010">outubro 2010</a></li>
+								<li><a href='' title="setembro 2010">setembro 2010</a></li>
+								<li><a href='' title="agosto 2010">agosto 2010</a></li>
+								<li><a href='' title="julho 2010">julho 2010</a></li>
+							</ul>
+						</li> 
+					</ul> 
+					
+				</div><!-- #sidebar -->
+				
+			</div><!-- #container -->
+			
+			<div id="footer">
+				<p>Tema <a href="http://www.wordpress.org">WordPress</a> desenvolvido por <a href="http://www.guiawp.com.br">Guia WordPress</a>.</p>
+			</div>
+						
+		</div><!-- #page-wrap -->
+
+	</body>
+</html>
